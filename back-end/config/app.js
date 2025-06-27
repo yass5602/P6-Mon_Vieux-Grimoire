e461d03cd3d3999fd4ec87265cd15620175fb583
yass5602 <yasinchk57@gmail.com>
2025-06-27 10:30:46 +0200

    Ajout de la dependance helmet

--- back-end/config/app.js
+++ back-end/config/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 const app = express();
 require("./../db/mongo.js");
 
 const IMAGES_FOLDER = String(process.env.IMAGES_FOLDER);
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      imgSrc: ["'self'", "data:", "http://localhost:4000"], // Ajoutez ici votre domaine et port
+      // ... autres directives si besoin
+    },
+  }));
 app.use(cors());
 app.use(express.json());
 app.use("/" + process.env.IMAGES_PUBLIC_URL, express.static(IMAGES_FOLDER));
