5b11d06ac52d25249d8be510e00e20ec15b94115
YAS <yasinchk57@gmail.com>
2025-06-29 12:41:10 +0200

    Time code ajouté dans le précédent commit pour gérer les conflit entre sharp et multer

--- back-end/middlewares/rateLimiter.js
+++ back-end/middlewares/rateLimiter.js
@@ -4,7 +4,7 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limite chaque IP à 100 requêtes par fenêtre
-  message: "Trop de requêtes, réessayez plus tard.",
+  message: "Trop de requêtes, réessayez plus tard",
 });
 
 module.exports = limiter;
